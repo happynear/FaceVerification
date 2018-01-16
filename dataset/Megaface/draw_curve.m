dc29f79efdb4dc1fa32cc0f4e60849ef2da82d19
happynear <wf900911@163.com>
2018-01-16 18:22:43 +0800

    update draw curve for MegaFace

--- dataset/Megaface/draw_curve.m
+++ dataset/Megaface/draw_curve.m
@@ -1,5 +1,5 @@
 result_folder = 'E:\datasets\MegaFace\results\';
-feature_suffix = '_sphere';
+feature_suffix = '_cos-m35';
 facescrub_cmc_file = ['cmc_facescrub_megaface' feature_suffix '_1000000_1.json'];
 facescrub_matches_file = ['matches_facescrub_megaface' feature_suffix '_1000000_1.json'];
 % facescrub_cmc_file = 'E:\datasets\MegaFace\results\Challenge1External\facenet\cmc_megaface_facescrub_1000000_1.json';
@@ -8,15 +8,20 @@ facescrub_cmc_json = loadjson(fileread([result_folder facescrub_cmc_file]));
 % facescrub_cmc_json = loadjson(fileread(facescrub_cmc_file));
 
 figure(1);
-semilogx(facescrub_cmc_json.cmc{1},facescrub_cmc_json.cmc{2},'LineWidth',2);
-title(['identification @ 1e6 distractors = ' num2str(facescrub_cmc_json.cmc{2}(1))]);
+semilogx(facescrub_cmc_json.cmc{1}+1,facescrub_cmc_json.cmc{2}*100,'LineWidth',2);
+title(['Identification @ 1e6 distractors = ' num2str(facescrub_cmc_json.cmc{2}(1))]);
+xlabel('Rank');
+ylabel('Identification Rate %');
 grid on;
 box on;
 hold on;
 
 figure(2);
 semilogx(facescrub_cmc_json.roc{1},facescrub_cmc_json.roc{2},'LineWidth',2);
-title(['verification @ 1e-6 = ' num2str(interp1(facescrub_cmc_json.roc{1}, facescrub_cmc_json.roc{2}, 1e-6))]);
+title(['Verification @ 1e-6 = ' num2str(interp1(facescrub_cmc_json.roc{1}, facescrub_cmc_json.roc{2}, 1e-6))]);
+xlim([1e-6 1]);
+xlabel('False Positive Rate');
+ylabel('True Positive Rate');
 grid on;
 box on;
 hold on;
